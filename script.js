92938ca912856bf19493f1062fb854f8507d2459
Omar <omardiaz55@gmail.com>
2025-05-11 14:25:54 -0400

    agregar boton subir

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function toggleMenu() {
   const navLinks = document.getElementById("navbarLinks");
   navLinks.classList.toggle("active");
 }
+
+const backToTopButton = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > window.innerHeight * 2) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+});
+
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
